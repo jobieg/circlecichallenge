0bca0f43c8b59fc373de3aca32b05f59ce49bd32
jobieg <50954357+jobieg@users.noreply.github.com>
2019-05-23 22:10:12 -0400

    Update test.42.js

--- test.42.js
+++ test.42.js
@@ -1,10 +1,26 @@
-var MSG = "42";
-describe("Show message tests", function() {
-  beforeEach(function() {
-    setUpHTMLFixture();
-    $('#txtMessage').val(MSG);
-    $('#btnShowMessage').trigger( "click" );
-  });
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Jasmine v2.0 DOM Tests Spec Runner</title>
+ 
+  <link rel="shortcut icon" type="image/png" href="images/jasmine_favicon.png">
+  <link rel="stylesheet" type="text/css" href="lib/jasmine-core/jasmine.css">
+ 
+  <script type="text/javascript" src="lib/jasmine-core/jasmine.js"></script>
+  <script type="text/javascript" src="lib/jasmine-core/jasmine-html.js"></script>
+  <script type="text/javascript" src="lib/jasmine-core/boot.js"></script>
+  <script type="text/javascript" src="lib/jasmine-core/jquery-1.7.2.js"></script>
+  <script type="text/javascript" src="lib/jasmine-core/jasmine-jquery.js"></script>
+   
+  <script type="text/javascript">
+    var MSG = "42";
+      describe("Show message tests", function() {
+        beforeEach(function() {
+          setUpHTMLFixture();
+            $('#txtMessage').val(MSG);
+            $('#btnShowMessage').trigger( "click" );
+     });
    
   it ("should display the message when button is clicked.", function() {
     expect($('#pMsg')).toHaveText($('#txtMessage').val());
@@ -17,8 +33,9 @@ describe("Hide message tests", function() {
     $('#pMsg').text(MSG);
     $('#btnHideMessage').trigger( "click" );
   });
-   
- // it ("should remove the message when button is clicked.", function() {
- //   expect($('#pMsg')).toHaveText("");
- // });
 });
+  </script>
+</head>
+<body>
+</body>
+</html>
